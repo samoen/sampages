aa27f91e10cf865850bc600a50c1e2d88dab9bb6
Sam Oen <samoen@gmail.com>
2023-05-25 12:38:07 +0800

    top bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,35 @@
-<p>Heyoooo</p>
-<slot></slot>
+<script>
+
+    import { base } from "$app/paths";
+
+</script>
+<svelte:head>
+    <title>Sam pages</title>
+</svelte:head>
+<div class="topbar">
+    <a href="{base}/">Home</a>
+    <a href="{base}/about">About</a>
+</div>
+
+<slot />
+
+<style>
+    .topbar{
+        background-color: aqua;
+        position: sticky;
+        top:0px;
+        padding:1rem;
+        display: flex;
+        gap:1rem;
+    }
+    a{
+
+    }
+    :global(body){
+        padding:0;
+        margin:0;
+    }
+    /* :global(html){
+        padding:0;
+    } */
+</style>
