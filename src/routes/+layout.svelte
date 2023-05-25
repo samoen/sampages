1ade87dd314e9439d204ac98a9b27c410d774db2
Sam Oen <samoen@gmail.com>
2023-05-25 12:59:59 +0800

    icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,35 @@
 <script>
-
     import { base } from "$app/paths";
-
+    import Search from "$lib/icons/Search.svelte";
 </script>
+
 <svelte:head>
     <title>Sam pages</title>
 </svelte:head>
 <div class="topbar">
     <a href="{base}/">Home</a>
     <a href="{base}/about">About</a>
+    <div class="spacer"></div>
+    <Search />
 </div>
 
 <slot />
 
 <style>
-    .topbar{
+    .topbar {
         background-color: aqua;
         position: sticky;
-        top:0px;
-        padding:1rem;
+        top: 0px;
+        padding: 1rem;
         display: flex;
-        gap:1rem;
+        gap: 1rem;
     }
-    a{
-
+    .spacer {
+        flex-grow: 1;
     }
-    :global(body){
-        padding:0;
-        margin:0;
+    :global(body) {
+        padding: 0;
+        margin: 0;
     }
     /* :global(html){
         padding:0;
